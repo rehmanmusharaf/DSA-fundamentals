802498a8a347dbda14ab9c6d4dce3736eb0c74ba
rehmanmusharaf <muhammadrehmanmusharafali@gmail.com>
2025-07-07 15:12:28 +0500

    feat:2d array

---
+++ 2darray/BasicWorking/2dArray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+// int arr[3][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+vector<vector<int>> arr;
+for(int i=0;i<3;i++)
+{
+    vector<int> subArr;
+    for(int j=0;j<4;j++)
+    {
+        subArr.push_back(j);
+    }
+    arr.push_back(subArr);
+}
+for(int i=0;i<3;i++)
+{
+    for(int j=0;j<4;j++)
+    {
+        cout<<"arr["<<i<<"]"<<"["<<j<<"]: "<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+}
+return 0;
+}
